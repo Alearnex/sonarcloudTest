ab406ab9a4683c9543ec733aa98da3d834c582cc
Alearnex <null>
2023-01-13 22:53:09 +0100

    First commit, trying to understand how to use git in Eclipse... And hopefully make it work with sonarcloud too.

---
+++ src/logic/Main.java
@@ -0,0 +1,10 @@
+package logic;
+
+//https://wiki.eclipse.org/EGit/User_Guide#Getting_Started
+
+public class Main {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello sonarcloud!");
+	}
+}
